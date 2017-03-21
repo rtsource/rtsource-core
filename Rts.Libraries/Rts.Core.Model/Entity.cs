a102ad0ab77299056dd19b3ef80abd126ec782a8
Bryan Loeper <bryan.loeper@gmail.com>
2017-03-20 20:24:34 -0500

    Changed libraries to support .NET Standard 1.6

--- Rts.Libraries/Rts.Core.Model/Entity.cs
+++ Rts.Libraries/Rts.Core.Model/Entity.cs
@@ -4,6 +4,8 @@ using System.Text;
 
 namespace Rts.Core.Model
 {
+    //TODO: Import https://github.com/bryanloeper/dotnet-booster/tree/develop and archive.
+
     public abstract class Entity
     {
         public Guid Id { get; set; }
