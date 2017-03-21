a33fd06d933faf6c5f00e01977272cf8074af522
Bryan Loeper <bryan.loeper@gmail.com>
2017-03-20 20:06:34 -0500

    Initial projects and entity models.

---
+++ Rts.Libraries/Rts.Core.Model/NamedEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rts.Core.Model
+{
+    public abstract class NamedEntity : Entity
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
